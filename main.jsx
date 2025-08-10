0b9ebef59e401dff7900efb1214e0597c4aced80
rahmankhudhair <rahman.khudhair@gmail.com>
2025-08-10 15:06:05 +0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import MCQPdfQuizApp from "./MCQPdfQuizApp.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MCQPdfQuizApp />
+  </React.StrictMode>
+);
